69db80d5e86b3dc96a36394852fdf3105cfeca62
nissano <nissano@wix.com>
2022-11-07 23:40:32 +0200

    9-error

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const x = fetch("https://rickandmortyapi.com/api/character/2")
+const x = fetch("https://rickandmortyapi.com/api/character/0")
 
 x
 .then(res => res.json())
 .then(x => console.log(x))
+.catch(e => console.error(e))
