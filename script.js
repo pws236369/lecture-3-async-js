ce8ea9d3e16fce14c331155f5e31dda94c8b8fc4
nissano <nissano@wix.com>
2022-11-07 23:44:35 +0200

    10-sync-then-fetch

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const x = fetch("https://rickandmortyapi.com/api/character/0")
+const x = fetch("https://rickandmortyapi.com/api/character/1")
 
 x
 .then(res => res.json())
 .then(x => console.log(x))
 .catch(e => console.error(e))
+
+// first, sharks.
+console.log("🦈🦈🦈🦈🦈🦈🦈")
